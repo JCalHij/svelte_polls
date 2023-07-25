664995e57b855c2119c3433dac25cdba22e79e5a
JCalHij <38001598+JCalHij@users.noreply.github.com>
2023-07-25 18:04:43 +0200

    Poll Form Component

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import Header from "./components/Header.svelte";
   import Footer from "./components/Footer.svelte";
   import Tabs from "./shared/Tabs.svelte";
+  import CreatePollForm from "./components/CreatePollForm.svelte";
 
   // Tabs
   const CURRENT_POLL_TAB = "Current Polls";
@@ -32,15 +33,7 @@
       est laborum.
     </p>
   {:else if activeItem === ADD_NEW_POLL_TAB}
-    <p style="color:red;">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </p>
+    <CreatePollForm/>
   {:else}
     <p>This should not be visible...</p>
   {/if}
---
+++ src/components/CreatePollForm.svelte
@@ -0,0 +1,42 @@
+<script>
+  let pollFields = {question: "", answerA: "", answerB: ""};
+
+  const onSubmitForm = () => {
+    console.log(pollFields);
+  };
+</script>
+
+<form on:submit|preventDefault={onSubmitForm}>
+  <div class="form-field">
+    <label for="question">Poll Question:</label>
+    <input type="text" id="question" bind:value={pollFields.question}>
+  </div>
+  <div class="form-field">
+    <label for="answer-a">Answer A:</label>
+    <input type="text" id="answer-a" bind:value={pollFields.answerA}>
+  </div>
+  <div class="form-field">
+    <label for="answer-b">Answer B:</label>
+    <input type="text" id="answer-b" bind:value={pollFields.answerB}>
+  </div>
+  <button>Add Poll</button>
+</form>
+
+<style>
+  form{
+    width: 400px;
+    margin: 0 auto;
+    text-align: center;
+  }
+  .form-field{
+    margin: 18px auto;
+  }
+  input{
+    width: 100%;
+    border-radius: 6px;
+  }
+  label{
+    margin: 10px auto;
+    text-align: left;
+  }
+</style>
